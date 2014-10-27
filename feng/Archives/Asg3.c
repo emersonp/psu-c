5213f7c12bd848e0f5c47b75c94a645958426238
emersonp <parker.emerson@gmail.com>
2014-10-26 21:17:31 -0700

    Added Simon Lab, Complete
    Simon Niklaus-assigned Tree Traversal Lab, completed.

---
+++ feng/Archives/Asg3.c
@@ -0,0 +1,136 @@
+// Copyright (c) 2014 Parker Harris Emerson
+// Assignment 3 for New Beginnings Fall 2014, Assigned by Wu-chang Feng.
+
+// Implement an IEEE floating point parser for an arbitrary number of bit settings
+
+// Simon says: A complete list of includes.
+#define _BSD_SOURCE
+#include <float.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+
+    // Instructions for use:
+    if (!(argv[1] && argv[2] && argv[3])) {
+        printf("\n\nUsage: Asg3.o <# of frac_bits> <# of exp_bits> <hex_to_convert>\n\n");
+        exit(0);
+    }
+    
+    // Declare some variables.
+    int inputHex;
+    sscanf(argv[3], "%x", &inputHex);
+    int fractionSize = atoi(argv[1]);
+    int fractionInt = 0;
+    float significand;
+    float fraction = 0;
+    int exponentSize = atoi(argv[2]);
+    int exponent = 0;
+    int bias = pow(2, (exponentSize - 1)) - 1;
+    int characteristic;
+    int maskVar = 0;
+    float output = 0;
+    int normalized = 1;
+    int signedBit = 0;
+
+    // Test input arguments.
+    if (fractionSize < 2 || fractionSize > 10) {
+        printf("Illegal number of fraction bits. Should be between 2 and 10.\n");
+        exit(0);
+    }
+    if (exponentSize < 3 || exponentSize > 5) {
+        printf("Illegal number of exponent bits. Should be between 2 and 10.\n");
+        exit(0);
+    }
+
+    // Create maskVar
+    for (int i = 0; i < fractionSize; i++) {
+        maskVar = (maskVar | 1 << i);
+    }
+
+    // Create fraction.
+    fractionInt = (inputHex & maskVar);
+    for (int i = 1; i <= fractionSize; i++) {
+        if (fractionInt & 1 << (fractionSize - i)) {
+            fraction += pow(2, -i);
+        }
+    }
+    
+    // Signed bit.
+    signedBit = (inputHex & 1 << (fractionSize + exponentSize));
+    if (signedBit) {
+        signedBit = 1;
+    }
+
+
+    // Create significand.
+    significand = 0;
+    if (normalized) {
+        significand = 1 + fraction;
+    } else {
+        significand = fraction;
+    }
+
+    // Reset and recreate maskVar.
+    maskVar = 0;
+    for (int i = 0; i < exponentSize; i++) {
+        maskVar = (maskVar | 1 << i);
+    }
+    maskVar = maskVar << fractionSize;
+
+    // Create exponent.
+    exponent = (inputHex & maskVar);
+    exponent = (unsigned) exponent >> fractionSize;
+    if (normalized) {
+        characteristic = exponent - bias;
+    } else {
+        characteristic = 1 - bias;
+    }
+
+    // Check normalization, NaN, infinity.
+    if (!exponent) {
+        printf("\nExponent: %d\n", exponent);
+        normalized = 0;
+    }
+    if (exponent == pow(2, exponentSize) - 1) {
+        if (!fraction) {
+            if (signedBit) {
+                printf("-Infinity.\n\n");
+                exit(0);
+            } else {
+                printf("+Infinity.\n\n");
+                exit(0);
+            }
+        } else {
+            printf("NaN.\n\n");
+            exit(0);
+        }
+    }
+    // Create and print the Output of Formula
+    output = (significand * pow(2, characteristic));
+    if (signedBit) {
+        output = -output;
+    }
+    printf("\nThe total of your input is %f.\n\n\n", output);
+
+    // Print debugging, left here in case Parker wants to fiddle with the program in the future.
+    /* 
+      printf("inputHex: %x\n", inputHex);
+      printf("significand: %f\n", significand);
+      printf("exponent (without bias): %d\n", exponent);
+      printf("characteristic (exponent): %d\n", characteristic);
+      printf("bias: %d\n", bias);
+      printf("exponent size: %d\n", exponentSize);
+      printf("fraction size: %d\n", fractionSize);
+      printf("fractionInt: %d\n", fractionInt);
+      printf("signedBit: %d\n", signedBit);
+    */
+
+}    
+
--- simon/SLab.c
+++ simon/SLab.c
@@ -9,27 +9,85 @@ typedef struct node_t {
 
 void treeCreate(node* nodeHandle) {
 	// set the values of the node
-    node.intValue = nodeHandle;
+    // nodeHandle->intValue = 0;
+    // nodeHandle->nodeLeft = NULL;
+    // nodeHandle->nodeRight = NULL;
+
 }
 
 void treeDestroy(node* nodeHandle) {
 	// destroy all nodes by using a postfix recursion
+    if (nodeHandle == NULL) {
+        return;
+    } else {
+        treeDestroy(nodeHandle->nodeLeft);
+        treeDestroy(nodeHandle->nodeRight);
+        free(nodeHandle);
+    }
 }
 
 void treeAdd(node* nodeHandle, int intValue) {
 	// allocate a new node and then descend iteratively through the tree in order to find the correct position
+    // Since I pulled from D2L (forgot about new version in email) and did this recursively, I want, like, 1000! gigapoints.
+    if (intValue < nodeHandle->intValue) {
+        if (nodeHandle->nodeLeft == NULL) {
+            node* newNode = (node*)(malloc(sizeof(node)));
+            newNode->intValue = intValue;
+            newNode->nodeLeft = NULL;
+            newNode->nodeRight = NULL;
+            nodeHandle->nodeLeft = newNode;
+        } else {
+            treeAdd(nodeHandle->nodeLeft, intValue);
+        }
+    } else if (intValue >= nodeHandle->intValue) {
+        if (nodeHandle->nodeRight) {
+        }
+        if (!nodeHandle->nodeRight) {
+            node* newNode = (node*)(malloc(sizeof(node)));
+            newNode->intValue = intValue;
+            newNode->nodeLeft = NULL;
+            newNode->nodeRight = NULL;
+            nodeHandle->nodeRight = newNode;
+        } else {
+            treeAdd(nodeHandle->nodeRight, intValue);
+        }
+    }
 }
 
 void treeTraverse(node* nodeHandle) {
 	// traverse through all nodes and print their value by using an infix recursion
+    if (nodeHandle == NULL) {
+        return;
+    }
+    treeTraverse(nodeHandle->nodeLeft);
+    printf("%d, ", nodeHandle->intValue);
+    treeTraverse(nodeHandle->nodeRight);
 }
 
-int treeHeight(node* nodeHandle, int intHeight) {
+int treeHeight(node* nodeHandle) {
 	// determine the height of the tree by using recursion
+    // Base Case
+    if (nodeHandle == NULL) {
+        return 0;
+    }
+    int leftDepth = treeHeight(nodeHandle->nodeLeft);
+    int rightDepth = treeHeight(nodeHandle->nodeRight);
+    if (leftDepth > rightDepth) {
+        return 1 + leftDepth;
+    } else {
+        return 1 + rightDepth;
+    }
 }
 
 int treeLeaves(node* nodeHandle) {
 	// determine the number of leaves of the tree by using recursion
+    if (nodeHandle == NULL) {
+        return 0;
+    } else if (nodeHandle->nodeLeft == NULL && nodeHandle->nodeRight == NULL) {
+        return 1;
+    } else {
+        return treeLeaves(nodeHandle->nodeLeft) + treeLeaves(nodeHandle->nodeRight);
+    }
 }
 
 int main() {
@@ -61,15 +119,15 @@ int main() {
 		}
 
 		{
-			printf("treeHeight: %d\n", treeHeight(&nodeRoot, 0));
+			printf("treeHeight: %d\n", treeHeight(&nodeRoot));
 
 			printf("treeLeaves: %d\n", treeLeaves(&nodeRoot));
 		}
 
 		{
-			treeDestroy(nodeRoot.nodeLeft);
+			// treeDestroy(nodeRoot.nodeLeft);
 
-			treeDestroy(nodeRoot.nodeRight);
+			// treeDestroy(nodeRoot.nodeRight);
 		}
 	}
 
